37ae2dc3602a4e9605d7723c6bcd28e773ede2e4
Grapeka <chotwilaiwan@gmail.com>
2023-01-12 14:50:13 +0700

    `feat` : create mock tasks

---
+++ src/mocks/tasks.js
@@ -0,0 +1,32 @@
+export const mockTask = [
+  {
+    id: 1,
+    title: 'Task 1',
+    day: 'Feb 5th at 2:30pm',
+    reminder: true,
+  },
+  {
+    id: 2,
+    title: 'Task 2',
+    day: 'Feb 6th at 2:30pm',
+    reminder: true,
+  },
+  {
+    id: 3,
+    title: 'Task 3',
+    day: 'Feb 7th at 1:30pm',
+    reminder: true,
+  },
+  {
+    id: 4,
+    title: 'Task 4',
+    day: 'Feb 9th at 6:30pm',
+    reminder: false,
+  },
+  {
+    id: 5,
+    title: 'Task 5',
+    day: 'Feb 11th at 6:30pm',
+    reminder: false,
+  },
+];
