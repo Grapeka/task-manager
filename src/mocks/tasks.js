2c5bfa3d2da59995bedc1b7134d622e144ea1d6c
Grapeka <chotwilaiwan@gmail.com>
2023-01-13 01:28:15 +0700

    `feat` : update mocks task time format

--- src/mocks/tasks.js
+++ src/mocks/tasks.js
@@ -2,31 +2,31 @@ export const mockTask = [
   {
     id: 1,
     title: 'Task 1',
-    day: 'Feb 5th at 2:30pm',
+    day: '1/1/2023',
     reminder: true,
   },
   {
     id: 2,
     title: 'Task 2',
-    day: 'Feb 6th at 2:30pm',
+    day: '1/2/2023',
     reminder: true,
   },
   {
     id: 3,
     title: 'Task 3',
-    day: 'Feb 7th at 1:30pm',
+    day: '1/3/2023',
     reminder: true,
   },
   {
     id: 4,
     title: 'Task 4',
-    day: 'Feb 9th at 6:30pm',
+    day: '1/4/2023',
     reminder: false,
   },
   {
     id: 5,
     title: 'Task 5',
-    day: 'Feb 11th at 6:30pm',
+    day: '1/5/2023',
     reminder: false,
   },
 ];
