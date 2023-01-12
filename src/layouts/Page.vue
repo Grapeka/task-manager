bbd55fbdf4faae36abbd28abdb3efaec4ad63f55
Grapeka <chotwilaiwan@gmail.com>
2023-01-13 02:38:52 +0700

    `feat` : create page layout

---
+++ src/layouts/Page.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PageLayout',
+  props: {},
+};
+</script>
+<style scope>
+.container {
+  margin-top: 25px;
+  width: 90vw;
+  border: 1px solid black;
+}
+</style>
